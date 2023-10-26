d87f37605023d2956f43a988c3be866e76912b02
ouacrime <104015090+ouacrime@users.noreply.github.com>
2023-10-26 15:20:54 +0100

    Ajoutez des fichiers projet.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _Db;
+
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _Db = Db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoeyList = _Db.Categories;
+
+            return View(objCategoeyList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _Db.Categories.Add(obj);
+                _Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
